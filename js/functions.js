6badd420ff129106d608e5dd958d0abe61485fd3
Mfoley196 <margofoley196@gmail.com>
2017-03-11 11:46:45 -0400

    add waypoints getter

--- js/functions.js
+++ js/functions.js
@@ -153,3 +153,14 @@ function getCurrentLocation() {
 var distance = function(){
     return google.maps.geometry.spherical.computeDistanceBetween(lastPos, currentPos);
 }
+
+function jsonCallback(json){
+   var waypoints = json.waypoints;
+}
+
+function getWaypoints(my_url){
+    $.ajax({
+        url: my_url,
+        dataType: "jsonp"
+    });
+}
